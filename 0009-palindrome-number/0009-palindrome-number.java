c22c0852e54f634dc7dd7ba18fdd2e5badd48043
Alain-RN <alainrakotoarivony926@gmail.com>
2025-08-02 01:48:52 +0300

    Time: 7 ms (14.11%), Space: 44.2 MB (35.78%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,7 +1,9 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        for(int i = 0; i < (""+x).length(); i++) {
-            if( (""+x).charAt(i) != (""+x).charAt((""+x).length() - i - 1) ) return false;
+        String s = ""+x;
+        int l = s.length();
+        for(int i = 0; i < l; i++) {
+            if( s.charAt(i) != s.charAt(l - i - 1) ) return false;
         }
         return true;
     }
