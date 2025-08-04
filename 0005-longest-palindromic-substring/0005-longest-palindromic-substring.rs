ee13877dcb61879f7a86796da2efd80630ffe9bb
Alain-RN <alainrakotoarivony926@gmail.com>
2025-08-04 18:45:49 +0300

    Time: 160 ms (15%), Space: 2.2 MB (42.5%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.rs
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.rs
@@ -11,12 +11,11 @@ impl Solution {
                     let i = chars[start].0;
                     let j = if end < chars.len() { chars[end].0 } else { input.len() };
                     let substring = &input[i..j];
-                    if substring == substring.to_string().chars().rev().collect::<String>() && substring.to_string().len() > sub_pal.len()  {
+                    if substring.chars().eq(substring.chars().rev()) && substring.len() > sub_pal.len() {
                         sub_pal = substring.to_string();
                     }
                 }
             }
-
             sub_pal
         }
     }
